e3bfd0c2d6ebff49c66210bf0820a4c62eef8624
Moe <moe@Moes-MacBook-Pro.local>
2019-09-18 19:38:56 -0400

    the sister

--- madlibs.py
+++ madlibs.py
@@ -6,6 +6,6 @@ print(verbs)
 
 #%%
 verbs = ["exclaims", "cross stitches", "eats ham"]
-print("The boy " + random.choice(verbs) + " all day long")
+print("The boy " + random.choice(verbs) + " all day long", "and his sister whatched him closely")
 
 #%%
