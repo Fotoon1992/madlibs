ba802e709cf8e615066876a7bc4055a2ece3a636
Moe <moe@Moes-MBP.attlocal.net>
2019-09-18 21:52:13 -0400

    chicken

--- madlibs.py
+++ madlibs.py
@@ -5,7 +5,7 @@ verbs = ["run", "jump", "plays"]
 print(verbs)
 
 #%%
-verbs = ["exclaims", "cross stitches", "eats ham"]
+verbs = ["exclaims", "cross stitches", "eats chicken"]
 print("The boy " + random.choice(verbs) + " all day long", "and his sister whatched him closely")
 
 #%%
